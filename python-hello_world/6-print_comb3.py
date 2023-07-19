a109483d6f52ad31f480c3d7b2270de54fa7d4a1
Gideon <giddysipoi@gmail.com>
2023-07-19 22:22:24 +0300

    updates

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,5 +1,2 @@
-for i in range(10):
-    for j in range(10):
-        if j!=i:
-            if i < j:
-                print('{}{}, '.format(i,j), end='\n')
+combinations = ['{}{}'.format(i, j) for i in range(10) for j in range(10) if i < j]
+print(', '.join(combinations))
