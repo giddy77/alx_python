76ca4e2f73d55f5eddddb455b86bd8ac6f2059a3
Gideon <giddysipoi@gmail.com>
2023-07-20 16:04:27 +0300

    updates

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -10,11 +10,11 @@ if number < 0 :
 print('last digit of {} is {} '.format( number,Last_digit), end="")
 
 if Last_digit > 5:
-    print('and is greater than 5')
+    print('and is greater than 5',end="\n")
 elif Last_digit == 0:
-    print('and is 0')
+    print('and is 0',end="\n")
 else:
-    print('and is less than 6 and not 0')
+    print('and is less than 6 and not 0',end="\n")
 
 
 
