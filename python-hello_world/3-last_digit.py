5eae00bf615454b142e62aebce71bee83d443a90
Gideon <giddysipoi@gmail.com>
2023-07-20 16:57:37 +0300

    updates

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import random
 
+
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 
-if number < 0:
-    sign = '-'
-else:
+if number >= 0:
     sign = ''
+else:
+    sign = '-'
 
 print('Last digit of {} is {}{}'.format(number, sign, last_digit), end=" ")
 
