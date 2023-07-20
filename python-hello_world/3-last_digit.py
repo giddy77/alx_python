f29060239900bba52e6dd46f06fdc92da9d37f16
Gideon <giddysipoi@gmail.com>
2023-07-20 14:49:34 +0300

    updates

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,20 +1,23 @@
-#!/usr/bin/python3
+# #!/usr/bin/python3
 import random
 
 number = random.randint(-10000, 10000)
+
+# Get the last digit of the number using the modulo operator (%)
 last_digit = abs(number) % 10
 
-if number < 0 and number!=0:
-    sign = '-'
-else:
-    sign = ''
+# Get the sign of the number
+sign = "-" if number < 0 else ""
 
-print('last digit of {} is {}{} '.format( number, sign,last_digit), end="")
+# Determine the output message based on the last digit and sign
+output_message = "The string Last digit of {}{} is {}".format(sign, abs(number), sign + str(last_digit))
 
 if last_digit > 5:
-    print('and is greater than 5')
+    output_message += " and is greater than 5"
 elif last_digit == 0:
-    print('and is 0')
+    output_message += " and is 0"
 else:
-    print('and is less than 6 and not 0')
+    output_message += " and is less than 6 and not 0"
 
+# Print the output message with a new line
+print(output_message)
