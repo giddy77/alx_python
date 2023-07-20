980188677323f0e103f9246eabc44252f959d9c5
Gideon <giddysipoi@gmail.com>
2023-07-20 16:09:23 +0300

    updates

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -7,7 +7,7 @@ Last_digit = abs(number) % 10
 if number < 0 :
     Last_digit = -Last_digit
     
-print('last digit of {} is {} '.format( number,Last_digit), end=" ")
+print('Last digit of {} is {} '.format( number,Last_digit), end=" ")
 
 if Last_digit > 5:
     print('and is greater than 5')
