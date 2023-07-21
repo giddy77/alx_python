2a5f548127f62c59c5b184511cf1762491815483
Gideon <giddysipoi@gmail.com>
2023-07-21 16:34:04 +0300

    updates

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,8 +1,12 @@
+def main():
+    convert_to_celsius(100)
+    convert_to_celsius(-40)
+    convert_to_celsius(-459.67)
+    convert_to_celsius(32)
+
 def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5/9
     return celsius
 
-print(convert_to_celsius(100))
-print(convert_to_celsius(-40))
-print(convert_to_celsius(-459.67))
-print(convert_to_celsius(32))
+
+main()
