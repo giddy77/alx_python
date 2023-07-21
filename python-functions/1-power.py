dd7b57de2694ea3bd92e09908dcae9fc9f37778e
Gideon <giddysipoi@gmail.com>
2023-07-21 16:31:20 +0300

    updates

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-
+def main():
+    pow(2, 2)
+    pow(98, 2)
+    pow(98, 0)
+    pow(100, -2)
+    pow(-4, 5)
+    
 def pow(a,b):
     return a**b
 
-
-print(pow(2, 2))
-print(pow(98, 2))
-print(pow(98, 0))
-print(pow(100, -2))
-print(pow(-4, 5))
+main()
