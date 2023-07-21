7a771fdd0382a5869382604c8409aa71d70f017b
Gideon <giddysipoi@gmail.com>
2023-07-21 16:14:30 +0300

    updates

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -5,14 +5,12 @@
 def add(a, b):
     return a + b
 
-# Test case: a = 1, b = 2
 result = add(1, 2)
 print(result)
 
-# Test case: a = 100, b = -2
+
 result = add(100, -2)
 print(result)
 
-# Test case: a = -100, b = -2
 result = add(-100, -2)
 print(result)
