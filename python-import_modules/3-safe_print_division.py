6d9957ba29e5ee7c47c075298d9593cea7e9d749
Gideon <giddysipoi@gmail.com>
2023-07-26 20:33:18 +0300

    updates

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -4,9 +4,7 @@ def safe_print_division(a, b):
      
     try:
         result = a / b
-    except ZeroDivisionError:
-        print("Error: Division by zero is not allowed.")
-        return None
+    
     except Exception as e:
         print("An error occurred:", e)
         return None
