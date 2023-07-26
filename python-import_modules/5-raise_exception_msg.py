46706f020bfae7dc7e631e824048f804b5fb3dcc
Gideon <giddysipoi@gmail.com>
2023-07-26 20:48:59 +0300

    updates

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,7 +1,4 @@
 def raise_exception_msg(message=""):
-    try:
-        raise_exception_msg("C is fun")
-    except NameError as ne:
-        print(ne)
+    raise NameError(message)
 
-raise_exception_msg("")
+raise_exception_msg()
