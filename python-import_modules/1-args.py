66f506c117ff56ed6cef8b562bca59b55d1f1f3d
Gideon <giddysipoi@gmail.com>
2023-07-26 20:40:37 +0300

    updates

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -3,7 +3,7 @@ import sys
 def main():
     num_args = len(sys.argv) - 1
     if num_args == 0:
-        print("0 argument.")
+        print("0 arguments.")
     else:
         print(f"{num_args} argument{'s' if num_args > 1 else ''}:")
         for i, arg in enumerate(sys.argv[1:], start=1):
