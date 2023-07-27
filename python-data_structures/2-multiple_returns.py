66ec6fe0ae4b8049d0c55c2540ce98cf35a251a7
Gideon <giddysipoi@gmail.com>
2023-07-27 22:42:24 +0300

    updates

---
+++ python-data_structures/2-multiple_returns.py
@@ -0,0 +1,9 @@
+sentence = 'The world'
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        return (None)
+    
+length = len(sentence)
+first = sentence[0]
+       
+print("Length :{} - First character: {}".format(length, first))
