5b74f25563484aaaf34dc55ec96b563d11593108
Gideon <giddysipoi@gmail.com>
2023-07-27 20:47:32 +0300

    updates

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,11 +1,18 @@
 def print_matrix_integer(matrix=[[]]):
+    if not matrix:
+        print()
+        return
+
     for row in matrix:
+        if not row:
+            print()
+            continue
+
         for i, num in enumerate(row):
             if i < len(row) - 1:
                 print("{:d}".format(num), end=" ")
             else:
                 print("{:d}".format(num))
 
-    if matrix == '':
-        return ''
+
 print_matrix_integer()
