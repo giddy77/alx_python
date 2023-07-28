041d1d0a61ef97f0b9a65c93e3919be661fa9c5a
Gideon <giddysipoi@gmail.com>
2023-07-28 10:24:50 +0300

    updates

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -7,8 +7,4 @@ def multiple_returns(sentence=""):
 
     
 result = multiple_returns()
-if result[0] is not None:
-    length, first = result
-    print("Length: {} - First character: {}".format(length, first))
-else:
-    print("Length: {} - First character: {}".format(result[0], result[1]))
+print("Length: {} - First character: {}".format(result[0], result[1]))
