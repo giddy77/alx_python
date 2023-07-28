189004e1d5bd357cba28eaf194de2a9a8513629b
Gideon <giddysipoi@gmail.com>
2023-07-28 10:22:31 +0300

    updates

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -2,6 +2,18 @@ def multiple_returns(sentence=""):
     if len(sentence) == 0:
         return (None, None)
     else:
-        print ("Length: {} - First character: {} ".format(len(sentence), sentence[0]))
-        
-multiple_returns()
+        return (len(sentence), sentence[0])
+
+result = multiple_returns()
+if result[0] is not None:
+    length, first = result
+    print("Length: {} - First character: {}".format(length, first))
+else:
+    print("Length: {} - First character: {}".format(result[0], result[1]))
+    
+result = multiple_returns()
+if result[0] is not None:
+    length, first = result
+    print("Length: {} - First character: {}".format(length, first))
+else:
+    print("Length: {} - First character: {}".format(result[0], result[1]))
