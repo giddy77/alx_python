b76840d8128df297d1ed3e3f23c885c1b10dc5be
Gideon <giddysipoi@gmail.com>
2023-07-31 19:47:45 +0300

    updates

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 def update_dictionary(a_dictionary, key, value):
     # Check if the key exists in the dictionary
     if key in a_dictionary:
@@ -6,3 +8,14 @@ def update_dictionary(a_dictionary, key, value):
     else:
         # Add the new key-value pair to the dictionary
         a_dictionary[key] = value
+
+# Define the dictionary
+my_dict = {'language': "C", 'number': 89, 'track': "Low level"}
+
+# Call the update_dictionary function
+update_dictionary(my_dict, 'language', "Python")
+
+# Sort and print the key-value pairs in the dictionary
+keys = sorted(my_dict.keys())
+for k in keys:
+    print("{}: {}".format(k, my_dict[k]))
