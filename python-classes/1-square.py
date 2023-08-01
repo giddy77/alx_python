b19dfc8e0321cc7d743b8e2f62dedaa87c05b599
Gideon <giddysipoi@gmail.com>
2023-08-01 22:22:21 +0300

    area of a square

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -16,13 +16,26 @@ class Square:
 
     """
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initialize a new Square instance.
 
         Args:
             size (int): The size of the square.
 
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
         self.__size = size
 
+    def area(self):
+        """Calculate the area of the square.
 
+        Returns:
+            int: The area of the square.
+
+        """
+        return self.__size ** 2
