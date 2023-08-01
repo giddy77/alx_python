e270512bda0b013dcdc1547fb7cb4791ef0da473
Gideon <giddysipoi@gmail.com>
2023-08-01 22:12:22 +0300

    documentation

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,24 @@
+"""This is a module-level documentation.
+
+This module contains a class called Square, which defines a square by its size.
+
+"""
 class Square:
     ...
+    """Represents a square.
+
+    This class defines a square by its size.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    """
     
-    def __init__(self,size):
+    def __init__(self, size):
+        """Initialize a new Square instance.
+
+        Arguments:
+            size (int): The size of the square.
+
+        """
         self.__size = size
