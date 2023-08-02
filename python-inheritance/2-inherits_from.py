a63ac59e4c6122c719a08fb0f200f98d3be40159
Gideon <giddysipoi@gmail.com>
2023-08-02 22:40:33 +0300

    PEP8 valid

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -1,6 +1,6 @@
-"""function that returns True if the object is an 
-instance of a class that inherited (directly or indirectly) from the specified class ;"""
 def inherits_from(obj, a_class):
-    """checks whether the object is an nstance of the class and whether is
-    a subclass and the obj is not an instance of the class itself"""
+    """
+    Checks whether the object is an instance of a class that inherited
+    (directly or indirectly) from the specified class.
+    """
     return isinstance(obj, type) or issubclass(type(obj), a_class) and type(obj) != a_class
