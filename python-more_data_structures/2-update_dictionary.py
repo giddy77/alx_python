d89a6422d8dd979005831d3bb868bddf806eea72
Gideon <giddysipoi@gmail.com>
2023-08-02 16:44:18 +0300

    updates

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,14 +1,3 @@
 def update_dictionary(a_dictionary, key, value):
-    # Check if the key exists in the dictionary
-    if key in a_dictionary:
-        # Update the existing key with the new value
-        a_dictionary[key] = value
-    else:
-        # Add the new key-value pair to the dictionary
-        a_dictionary[key] = value
-
-    # Sort the dictionary by keys and print the key-value pairs
-    sorted_dict = {k: a_dictionary[k] for k in sorted(a_dictionary)}
-    for k in sorted_dict:
-        print("{}: {}".format(k, sorted_dict[k]))
-
+    a_dictionary[key] = value
+    return a_dictionary
