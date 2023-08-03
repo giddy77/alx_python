e4c25f50c1db74922e381ff6972219ee08e0bb9c
Gideon <giddysipoi@gmail.com>
2023-08-03 22:27:43 +0300

    rectangle

---
+++ python-inheritance/6-rectangle.py
@@ -0,0 +1,23 @@
+"""this is an empty class"""
+class BaseGeometry:
+    """this is a place holder for this empty class"""
+    
+    def area(self):
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise Exception(f'{name} must be an integer')
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+
+class Rectangle(BaseGeometry):
+    
+    def __init__(self, width, hieght):
+        self.__width = width
+        self.__hieght = hieght
+        
+        self.integer_validator("width", width)
+        self.integer_validator("hieght", hieght)
