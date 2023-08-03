194a356ad0f7c9cef308a96d72ffbaef3a339328
Gideon <giddysipoi@gmail.com>
2023-08-03 22:02:38 +0300

    updates

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,6 @@
+"""this is an empty class"""
+class BaseGeometry:
+    """this is a place holder for this empty class"""
+    
+    def area(self):
+        raise Exception("area() is not implemented")
