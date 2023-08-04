24405c4103581c988683d33272f66c3fc197da81
Gideon <giddysipoi@gmail.com>
2023-08-04 23:37:09 +0300

    updates

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -5,9 +5,6 @@ class metaclassbase(type):
     """this is my class"""
 class BaseGeometry(metaclass = metaclassbase):
     """this is a place holder for this empty class"""
-    def __dir__(cls):
-        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-
     
     def area(self):
         raise Exception("area() is not implemented")
