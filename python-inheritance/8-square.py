58905c1d924de35a6d3304b40ece030ccc4b9473
Gideon <giddysipoi@gmail.com>
2023-08-05 00:31:16 +0300

    square

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,38 +1,32 @@
-"""this is an empty class"""
 class metaclassbase(type):
-    def __dir__(cls):
- 
-        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-"""this is my class"""
-class BaseGeometry(metaclass = metaclassbase):
-    """this is a place holder for this empty class"""
     def __dir__(cls):
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
 
+"""this is my class"""
+class BaseGeometry(metaclass=metaclassbase):
+    """This is a place holder for this empty class"""
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
     
     def area(self):
         raise Exception("area() is not implemented")
-
     
     def integer_validator(self, name, value):
         if not isinstance(value, int):
             raise TypeError(f'{name} must be an integer')
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
-        
 
-
-"""this isthe rectangle class"""
+"""this is the rectangle class"""
 class Rectangle(BaseGeometry):
     """This is a class that inherits from the BaseGeometry Class"""
     def __init__(self, width, height):
         self.__width = width
         self.__height = height
         self.integer_validator("width", width)
-        self.integer_validator("height", height)  # Corrected parameter name
-
+        self.integer_validator("height", height)
         
-        """this is the area"""
+    """this is the area"""
     def area(self):
         return self.__width * self.__height
     
