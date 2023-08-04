eeb96fc00180824f053b4dcc0f705fa02051fa3d
Gideon <giddysipoi@gmail.com>
2023-08-05 00:05:56 +0300

    updates

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -27,3 +27,5 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
       
         
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)  
