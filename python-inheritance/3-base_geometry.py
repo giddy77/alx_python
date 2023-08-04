c93adec231f66201ff8895eac5e12c4627a35a59
Gideon <giddysipoi@gmail.com>
2023-08-04 23:02:48 +0300

    updates

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,8 +1,9 @@
 """this is an empty class"""
-class BaseGeometry:
+class metaclass:
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__ if attribute != '__init_subclass__']
+    """this is my class"""
+class BaseGeometry(metaclass):
     """this is a place holder for this empty class"""
-    def __init__(self):
-        """this"""
-        pass
-
-BaseGeometry()
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__ if attribute != '__init_subclass__']
