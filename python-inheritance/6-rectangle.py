f71aff5dc36ebd135c0aa68a22f1986e0287ea47
Gideon <giddysipoi@gmail.com>
2023-08-04 23:41:01 +0300

    updates

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -1,15 +1,24 @@
 """this is an empty class"""
-class BaseGeometry:
+class metaclassbase(type):
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    """this is my class"""
+class BaseGeometry(metaclass = metaclassbase):
     """this is a place holder for this empty class"""
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
     
     def area(self):
         raise Exception("area() is not implemented")
+
     
     def integer_validator(self, name, value):
         if not isinstance(value, int):
-            raise Exception(f'{name} must be an integer')
+            raise TypeError(f'{name} must be an integer')
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
+        
 
 
 
