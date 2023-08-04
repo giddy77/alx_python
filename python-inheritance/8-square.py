b6a459921d0c072b53787ac5ebda1dc60fd61fc6
Gideon <giddysipoi@gmail.com>
2023-08-05 00:17:38 +0300

    square

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,42 @@
+"""this is an empty class"""
+class metaclassbase(type):
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+    """this is my class"""
+class BaseGeometry(metaclass = metaclassbase):
+    """this is a place holder for this empty class"""
+    def __dir__(cls):
+        return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
+
+    
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError(f'{name} must be an integer')
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        
+
+
+
+class Rectangle(BaseGeometry):
+    """This is a class that inherits from the BaseGeometry Class"""
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)  # Corrected parameter name
+
+        
+        
+    def area(self):
+        return self.__width * self.__height
+    
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def __repr__(self):
+        return f"Rectangle({self.__width}, {self.__height})"
