19b3b80ed48e0e1fcb66c2bed88c60cc2fb03dab
Gideon <giddysipoi@gmail.com>
2023-08-05 00:28:45 +0300

    square

--- python-inheritance/8-square.py
+++ python-inheritance/8-square.py
@@ -1,8 +1,9 @@
 """this is an empty class"""
 class metaclassbase(type):
     def __dir__(cls):
+ 
         return [attribute for attribute in super().__dir__() if attribute != '__init_subclass__']
-    """this is my class"""
+"""this is my class"""
 class BaseGeometry(metaclass = metaclassbase):
     """this is a place holder for this empty class"""
     def __dir__(cls):
