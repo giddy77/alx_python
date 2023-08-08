253c3479d8cba08b3dfa530da06f6ad192d01e54
Gideon <giddysipoi@gmail.com>
2023-08-08 22:42:58 +0300

    updates

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -10,32 +10,32 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
     
-    """Getter and setter for width"""
+    """Getter  for width"""
     def get_width(self):
         return self.__width
-    
+    """setter for width"""
     def set_width(self, width):
         if width > 0:
             self.__width = width
     
-    """Getter and setter for height"""
+    """Getter  for height"""
     def get_height(self):
         return self.__height
-    
+    """setter for width"""
     def set_height(self, height):
         if height > 0:
             self.__height = height
     
-    # Getter and setter for x
+    """Getter  for x"""
     def get_x(self):
         return self.__x
-    
+    """setter for x"""
     def set_x(self, x):
         self.__x = x
     
-    # Getter and setter for y
+    """Getter for y"""
     def get_y(self):
         return self.__y
-    
+    """setter for y"""
     def set_y(self, y):
         self.__y = y
