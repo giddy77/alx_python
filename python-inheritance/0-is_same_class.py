b0521d604922bb7e7f30fd8ee5b0582aa9887a83
Gideon <giddysipoi@gmail.com>
2023-08-08 16:45:14 +0300

    updates

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -2,7 +2,13 @@
 an instance of the same class"""
 def is_kind_of_class(obj, a_class):
     """this checks whether the obj is 
-    an instance of the same class"""
+    an instance of the same class
+    
+    """
+    
+    
+    
+    
     return isinstance(obj, a_class)
 
         
