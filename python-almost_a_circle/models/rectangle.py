7245ccfe0efd0ca9ff3f32bd16dccba5b3c6cae6
Gideon <giddysipoi@gmail.com>
2023-08-10 19:06:39 +0300

    updates

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,8 +2,9 @@
 from .base import Base
 """this is definition of class Rectangle"""
 class Rectangle(Base):
-    """here we have the constructor of the Rectangle Class"""
+    
     def __init__(self, width, height, x=0, y=0, id=None):
+        """here we have the constructor of the Rectangle Class"""
         self.__width = self.__validate_integer_positive("width", width)
         self.__height = self.__validate_integer_positive("height", height)
         self.__x = self.__validate_integer_nonnegative("x", x)
