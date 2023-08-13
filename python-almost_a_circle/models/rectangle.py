6efdef4962a5df2e60fab7886024e069d14030f0
Gideon <giddysipoi@gmail.com>
2023-08-13 22:54:07 +0300

    updates

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,65 +1,65 @@
 from models.base import Base
 
 class Rectangle(Base):
-    def __init__(self, width, height, x=0, y=0, id=None):
-        super().__init__(id)  # Use __init__ instead of init
-        self._width = width   # Use private attribute names to avoid naming conflicts
-        self._height = height
-        self._x = x
-        self._y = y
+    def init(self, width, height, x=0, y=0, id=None):
+        super().init(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
-        return self._width
+        return self.width
 
     @width.setter
     def width(self, value):
         if isinstance(value, int) and value > 0:
-            self._width = value
+            self.width = value
         else:
             raise ValueError("Width must be a positive integer")
 
     @property
     def height(self):
-        return self._height
+        return self.height
 
     @height.setter
     def height(self, value):
         if isinstance(value, int) and value > 0:
-            self._height = value
+            self.height = value
         else:
             raise ValueError("Height must be a positive integer")
 
     @property
     def x(self):
-        return self._x
+        return self.x
 
     @x.setter
     def x(self, value):
         if isinstance(value, int):
-            self._x = value
+            self.x = value
         else:
             raise ValueError("X must be an integer")
 
     @property
     def y(self):
-        return self._y
+        return self.y
 
     @y.setter
     def y(self, value):
         if isinstance(value, int):
-            self._y = value
+            self.y = value
         else:
             raise ValueError("Y must be an integer")
 
     def area(self):
-        return self._width * self._height
+        return self.width * self.height
 
     def display(self):
-        for _ in range(self._y):
+        for _ in range(self.__y):
             print()
-        for _ in range(self._height):
-            print(" " * self._x + "#" * self._width)
+        for _ in range(self.height):
+            print(" " * self.x + "#" * self.width)
 
-    def __str__(self):  # Use __str__ instead of str
-        return f"[Rectangle] ({self.id}) {self._x}/{self._y} - {self._width}/{self._height}"
+    def str(self):
+        return f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.__height}"
