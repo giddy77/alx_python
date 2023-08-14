7510a9835da8ef6eb602c57b007364c5c0f3611a
Gideon <giddysipoi@gmail.com>
2023-08-15 00:46:48 +0300

    updates

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,12 +3,10 @@ import requests
 """this is first use of requests modules to make requests to the server"""
 url = "https://alu-intranet.hbtn.io/status"
 response = requests.get(url)
+"""access the properties of that object response"""
+content = response.status_code
+strtype = response.reason
 
-if response.status_code == 200:
-    """if the request is a success"""
-    data = response.json()
+print("-" + strtype)
+print("-" + str(content))
 
-    print("\t- type:", type(data))
-    print("\t- content:", data)
-else:
-    print("Request to", url, "failed with status code:", response.status_code)
