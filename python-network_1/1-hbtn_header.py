67256a3e376d868c1cb6872840d5f7e06560d229
Gideon <giddysipoi@gmail.com>
2023-08-15 01:10:27 +0300

    updates

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -3,7 +3,7 @@ import sys
 
 url = input("url: ")
 
-request = requests(url)
 
-X_Request_Id = request.headers.get("X-Request-Id")
-print(X_Request_Id)
+def getheader(url):
+    request = requests(url)
+    return request.headers.get('X-Request-Id')
