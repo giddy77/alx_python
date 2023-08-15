34b647ee1d2bb8895012a63566e6add7f8f0fb93
Gideon <giddysipoi@gmail.com>
2023-08-15 18:50:47 +0300

    updates

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -5,10 +5,10 @@ def main():
     url = sys.argv[1]
     email = sys.argv[2]
     
-    response = requests.post(url,email)
+    response = requests.post(url,data={'email':email})
     
     email = response.text
     
-    print(f"Email: {email}")
+    print(email)
 if __name__=="__main__":
     main()
