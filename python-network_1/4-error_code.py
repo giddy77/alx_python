f403aa39bedf3ecd84b37d4d28ab0225c936108e
Gideon <giddysipoi@gmail.com>
2023-08-15 18:56:59 +0300

    error-code

---
+++ python-network_1/4-error_code.py
@@ -0,0 +1,13 @@
+import requests
+import sys
+
+
+def main():
+    url = sys.argv[1]
+    
+    response = requests.get(url)
+    responsebody = response.content
+    print(responsebody)
+    
+    if response.status_code > 400:
+        print("Error code: {}".format(response.status_code))
