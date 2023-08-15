3aca983d6cf87e110ca35a57d50a746402e229ca
Gideon <giddysipoi@gmail.com>
2023-08-15 19:26:18 +0300

    updates

---
+++ python-network_1/6-my_github.py
@@ -0,0 +1,20 @@
+import requests
+import sys
+
+def main():
+    username = 'giddy77'
+    password = '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'
+
+    url = "https://api.github.com/user"
+
+    response = requests.get(url, auth=(username, password))
+
+    if response.status_code == 200:
+        user_data = response.json()
+        user_id = user_data['id']
+        print("User ID:", user_id)
+    else:
+        print("Request failed with status code:", response.status_code)
+
+if __name__ == "__main__":
+    main()
