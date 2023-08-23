203964c8a8b339755ebfe850dcc6887211532c3e
gidd77 <giddysipoi@gmail.com>
2023-08-24 00:31:40 +0300

    updates

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python3
 """
-Script that takes in an argument and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument.
+Script that lists all cities from the database hbtn_0e_4_usa.
 """
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Check if all 4 arguments are provided
-    if len(sys.argv) != 5:
-        print("Usage: {} username password database_name state_name_searched".format(sys.argv[0]))
+    # Check if all 3 arguments are provided
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database_name".format(sys.argv[0]))
         sys.exit(1)
-        
+
     username = sys.argv[1]
     password = sys.argv[2]
     database_name = sys.argv[3]
-    state_name_searched = sys.argv[4]
 
     try:
         # Connect to the MySQL server
         db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database_name)
         cursor = db.cursor()
 
-        """use parameters query to 
-        ensure the input is treated as data and not query"""
-        query = "SELECT * FROM cities WHERE name = %s ORDER BY id"
-        cursor.execute(query, (state_name_searched,))
+        # Create and execute the SQL query
+        query = "SELECT * FROM cities ORDER BY cities.id ASC"
+        cursor.execute(query)
 
         # Fetch and display the results
         results = cursor.fetchall()
