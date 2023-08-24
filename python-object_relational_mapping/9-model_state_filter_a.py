3b92ccf1822281a47f73d177bb3e730d321123c1
gidd77 <giddysipoi@gmail.com>
2023-08-24 22:10:37 +0300

    updates

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Script that lists all State objects from the database hbtn_0e_6_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database_name".format(sys.argv[0]))
+        sys.exit(1)
+    
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database_name = sys.argv[3]
+    
+    # Create an engine to connect to the MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database_name))
+    
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    
+    # Retrieve all State objects and sort by states.id in ascending order
+    states = session.query(State).filter(State.name.like('%a%')).order_by(State.id).all()
+    
+    
+    # Display the results
+  
+     # Display the results
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
+    
+    # Close the session
+    session.close()
