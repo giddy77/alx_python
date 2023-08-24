a3c9db6747054be186595a80881ff27fafcde6c5
gidd77 <giddysipoi@gmail.com>
2023-08-24 20:47:29 +0300

    updates

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
             ensure the input is treated as data and not query
         """
         query = query = (
-           "SELECT * FROM cities LEFTJOIN states ON cities.id  = cities.state_id WHERE states.name = '%s' ORDER BY cities.id ASC"
+           "SELECT * FROM cities LEFTJOIN states ON cities.state_id  = states.id WHERE states.name = '%s' ORDER BY cities.id ASC"
         )
         cursor.execute(query, (state_name_searched,))
 
