57751e2a0c0d65ef5e964fb35853b5ab892961c0
gidd77 <giddysipoi@gmail.com>
2023-08-24 21:27:08 +0300

    updates

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+    def __init__(self, name):
+        self.name = name
