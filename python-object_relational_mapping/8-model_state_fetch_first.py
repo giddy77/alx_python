b9a1e9806ddddb79da1c5999fcc9f98c67570315
gidd77 <giddysipoi@gmail.com>
2023-08-25 09:39:59 +0300

    corrections

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -28,8 +28,10 @@ if __name__ == "__main__":
     state = session.query(State).order_by(State.id).first()
     
     # Display the results
-  
-    print("{}: {}".format(state.id, state.name))
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("No states found.")
     
     # Close the session
     session.close()
