dc79ef511356119f15ece72336b93fd7cdc0c54d
giddy77 <giddysipoi@gmail.com>
2023-08-30 19:38:22 +0300

    updates

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route("/",strict_slashes = False)
 def main():
     """here is the function that executes when that route is hit"""
-    print("Hello HBNB!")
+    return "Hello HBNB!"
 
 if __name__ == "__name__":
-    app.run(host="127.0.0.0.0",port = 5000)
+    app.run(host="0.0.0.0.0",port = 5000)
