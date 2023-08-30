2209114c860ffeb9777f780a59404a1f7672515d
giddy77 <giddysipoi@gmail.com>
2023-08-30 20:47:34 +0300

    interger test

---
+++ python-web_framework/4-number_route.py
@@ -0,0 +1,39 @@
+"""here we import the flask module"""
+from flask import Flask
+
+"""we intialise the Flask module"""
+app = Flask(__name__)
+
+
+"""we declare the route of the resource"""
+@app.route("/", strict_slashes = False)
+def main():
+    """here is the function that executes when that route is hit"""
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes = False)
+def first():
+    """this is the function"""
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes = False)
+def second(text):
+    """this is text"""
+    modified = "C " + text.replace('_', ' ')
+    return modified
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes = False)
+def third(text="is cool"):
+    """this is the third function"""
+    modified = "Python "+text.replace('_', ' ')
+    return modified
+
+@app.route("/number/<n>:", strict_slashes = False)
+def interger_check(text):
+    """returns this is an interger if the number is an interger"""
+    check = type(text,'int')
+    if check:
+        return "This is an interger"
+    
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port = 5000)
