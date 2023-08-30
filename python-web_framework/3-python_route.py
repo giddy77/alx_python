e62f2e8d44b304f8e6083c4e1395db4297df587d
giddy77 <giddysipoi@gmail.com>
2023-08-30 20:17:57 +0300

    corrections

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -24,7 +24,7 @@ def second(text):
 @app.route("/python/<text>", strict_slashes = False)
 def third(text):
     """the third function"""
-    modified = "Python " + text.replace("_"," ")
+    modified = "Python is cool " + text.replace("_"," ")
     return modified
     
 if __name__ == "__main__":
