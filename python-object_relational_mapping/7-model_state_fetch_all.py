db1d7583c390f6052ebef98de07bde201ff072de
giddy77 <giddysipoi@gmail.com>
2023-10-01 20:57:43 +0300

    corrections

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that lists all State objects from the database hbtn_0e_6_usa
+Module to link class to table in database and display State objects.
 """
 
 import sys
@@ -9,27 +9,30 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
+    # Check for correct number of arguments
     if len(sys.argv) != 4:
-        print("Usage: {} username password database_name".format(sys.argv[0]))
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
-    
+
     username = sys.argv[1]
     password = sys.argv[2]
-    database_name = sys.argv[3]
-    
-    # Create an engine to connect to the MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database_name))
-    
+    database = sys.argv[3]
+
+    # Create an engine
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(username, password, database),
+        pool_pre_ping=True
+    )
+
     # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
-    
-    # Retrieve all State objects and sort by states.id in ascending order
+
+    # Query and display State objects
     states = session.query(State).order_by(State.id).all()
-    
-    # Display the results
     for state in states:
         print("{}: {}".format(state.id, state.name))
-    
+
     # Close the session
     session.close()
