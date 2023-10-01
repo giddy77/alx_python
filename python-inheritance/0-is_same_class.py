5f47257f13f3bce7047da81ce8c1a0852d6df60d
giddy77 <giddysipoi@gmail.com>
2023-10-01 21:02:21 +0300

    corrections

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,17 +1,16 @@
-"""this checks whether the obj is 
-an instance of the same class
+#!/usr/bin/python3
+"""python inheritance.
+
+defines and assigns value for parameter
+
 """
+def is_same_class(obj, a_class):
+    """Check if the object is exactly an instance of the specified class.
+    Args:
+        obj: The object to check.
+        a_class: The class to compare against.
 
-
-def is_kind_of_class(obj, a_class):
-    """this checks whether the obj is 
-    an instance of the same class
-    
+    Returns:
+        True if the object is exactly an instance of the specified class; otherwise, False.
     """
-    
-    
-    
-    
-    return isinstance(obj, a_class)
-
-        
+    return type(obj) is a_class
