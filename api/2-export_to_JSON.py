b3017c4b645aa45448fbeea47b210aa2ee864f4d
giddy77 <giddysipoi@gmail.com>
2023-10-11 13:24:31 +0300

    updates

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,20 +1,19 @@
-#this is the export to json module
-import requests
-import sys
-import json
+import requests  # Import the requests module for making HTTP requests
+import sys       # Import the sys module to access command-line arguments
+import json      # Import the json module for JSON handling
 
-#this is the export to json module
 def get_employee_todo_list_progress(employee_id):
     # Fetch employee details
     employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
     response = requests.get(employee_url)
     employee_data = response.json()
 
+    # Check if the 'name' key exists in the employee data
     if 'name' not in employee_data:
         print(f"Employee with ID {employee_id} not found.")
         return
 
-    employee_name = employee_data['name']
+    employee_name = employee_data['name']  # Get the employee's name
 
     # Fetch employee's TODO list
     todo_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
@@ -22,8 +21,8 @@ def get_employee_todo_list_progress(employee_id):
     todo_data = response.json()
 
     # Calculate the progress
-    total_tasks = len(todo_data)
-    completed_tasks = sum(1 for task in todo_data if task['completed'])
+    total_tasks = len(todo_data)  # Total number of tasks
+    completed_tasks = sum(1 for task in todo_data if task['completed'])  # Number of completed tasks
 
     # Create a data structure for JSON export
     export_data = {
